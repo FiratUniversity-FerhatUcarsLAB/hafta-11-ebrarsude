b7e924243acd031e40c02e062ab12dc56d45fd51
ebrarsude <250541104@firat.edu.tr>
2025-12-03 10:01:24 +0300

    Update Exercise4_1.java

--- Exercise4_1.java
+++ Exercise4_1.java
@@ -1,14 +1,21 @@
 public class Exercise4_1 {
 
     public static void printAmerican(String day, int date, String month, int year) {
-        // TODO
+      // Ornek: Carsamba, aralik 3 , 2025
+        System.out.println(day + ", " + month + " " + date + ", " + year);
     }
 
     public static void printEuropean(String day, int date, String month, int year) {
-        // TODO
+        // // Ornek: 3 Aralik 2025, Carsamba
+        System.out.println(date + " " + month + " " + year + ", " + day);
     }
 
     public static void main(String[] args) {
         // TODO: test methods
+        System.out.println("Amerikan Formati");
+        printAmerican("Carsamba", 3, "Aralik", 2025);
+
+        System.out.println("Avrupa Formati");
+        printEuropean("Carsamba", 3, "Aralik", 2025);
     }
 }
